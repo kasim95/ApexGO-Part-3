060c07c724809a0a98f120d6eb38df2641977e87
amrazek <amrazek@csu.fullerton.edu>
2019-11-07 03:17:00 -0800

    fixed looping issue in web_cnn multiprocessing

---
+++ set_tf_gpu_env.bat
@@ -0,0 +1,4 @@
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin;%PATH%
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\extras\CUPTI\libx64;%PATH%
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\include;%PATH%
+SET PATH=F:\cuda\bin;%PATH%
--- web_cnn.py
+++ web_cnn.py
@@ -6,18 +6,26 @@ from dlgo.encoders.sevenplane import SevenPlaneEncoder
 from dlgo.networks import large
 import h5py
 
-go_board_rows, go_board_cols = 19, 19
-nb_classes = go_board_rows * go_board_cols
-encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
-processor = GoDataProcessor(encoder=encoder.name())
-X, y = processor.load_go_data(num_samples=100)
-input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
-model = Sequential()
-network_layers = large.layers(input_shape)
-for layer in network_layers:
-    model.add(layer)
-model.add(Dense(nb_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-model.fit(X, y, batch_size=128, epochs=20, verbose=1)
-deep_learning_bot = DeepLearningAgent(model, encoder)
-deep_learning_bot.serialize(h5py.File("agents/deep_bot.h5", 'w'))
+
+def run():
+    go_board_rows, go_board_cols = 19, 19
+    nb_classes = go_board_rows * go_board_cols
+    encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
+    processor = GoDataProcessor(encoder=encoder.name())
+    X, y = processor.load_go_data(num_samples=100)
+    input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
+    model = Sequential()
+    network_layers = large.layers(input_shape)
+    for layer in network_layers:
+        model.add(layer)
+    model.add(Dense(nb_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    model.fit(X, y, batch_size=128, epochs=20, verbose=1)
+    deep_learning_bot = DeepLearningAgent(model, encoder)
+    deep_learning_bot.serialize(h5py.File("agents/deep_bot.h5", 'w'))
+
+
+if __name__ == '__main__':
+    import multiprocessing
+    multiprocessing.freeze_support()
+    run()
