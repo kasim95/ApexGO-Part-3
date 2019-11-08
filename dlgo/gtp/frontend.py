e38c62e20ff03399f55d5f6e44793dbc9e36e4d0
amrazek <amrazek@csu.fullerton.edu>
2019-11-08 12:57:12 -0800

    implemented gtp/frontend

---
+++ dlgo/gtp/frontend.py
@@ -0,0 +1,129 @@
+from __future__ import absolute_import
+import sys
+from dlgo.gtp import command, response
+from dlgo.gtp.board import coords_to_gtp_position, gtp_position_to_coords
+from dlgo.goboard_fast import GameState, Move
+from utils import print_board
+
+__all__ = ['GTPFrontend']
+
+HANDICAP_STONES = {
+    2: ['D4', 'Q16'],
+    3: ['D4', 'Q16', 'D16'],
+    4: ['D4', 'Q16', 'D16', 'Q4'],
+    5: ['D4', 'Q16', 'D16', 'Q4', 'K10'],
+    6: ['D4', 'Q16', 'D16', 'Q4', 'D10', 'Q10'],
+    7: ['D4', 'Q16', 'D16', 'Q4', 'D10', 'Q10', 'K10'],
+    8: ['D4', 'Q16', 'D16', 'Q4', 'D10', 'Q10', 'K4', 'K16'],
+    9: ['D4', 'Q16', 'D16', 'Q4', 'D10', 'Q10', 'K4', 'K16', 'K10'],
+}
+
+
+class GTPFrontend:
+    def __init__(self, termination_agent, termination=None):
+        self.agent = termination_agent
+        self.game_state = GameState.new_game(19)
+        self._input = sys.stdin
+        self._output = sys.stdout
+        self._stopped = False
+
+        self.handlers = {
+            'boardsize': GTPFrontend.handle_boardsize,
+            'clear_board': self.handle_clear_board,
+            'fixed_handicap': self.handle_fixed_handicap,
+            'genmove': self.handle_genmove,
+            'known_command': self.handle_known_command,
+            'komi': GTPFrontend.ignore,
+            'showboard': self.handle_showboard,
+            'time_settings': GTPFrontend.handle_time_settings,
+            'time_left': GTPFrontend.handle_time_left,
+            'play': self.handle_play,
+            'protocol_version': GTPFrontend.handle_protocol_version,
+            'quit': self.handle_quit,
+        }
+
+    def run(self):
+        while not self._stopped:
+            input_line = self._input.readline().strip()
+            cmd = command.parse(input_line)
+            resp = self.process(cmd)
+
+            self._output.write(response.serialize(cmd, resp))
+            self._output.flush()
+
+    def process(self, cmd):
+        handler = self.handlers.get(cmd.name, self.handle_unknown)
+        return handler(*cmd.args)
+
+    def handle_play(self, color, move):
+        if move.lower() == 'pass':
+            self.game_state = self.game_state.apply_move(Move.pass_turn())
+        elif move.lower() == 'resign':
+            self.game_state = self.game_state.apply_move(Move.resign())
+        else:
+            self.game_state = self.game_state.apply_move(gtp_position_to_coords(move))
+        return response.success()
+
+    def handle_genmove(self, color):
+        move = self.agent.select_move(self.game_state)
+        self.game_state = self.game_state.apply_move(move)
+        if move.is_pass:
+            return response.success('pass')
+        if move.is_resign:
+            return response.success('resign')
+        return response.success(coords_to_gtp_position(move))
+
+    def handle_fixed_handicap(self, nstones):
+        nstones = int(nstones)
+        for stone in HANDICAP_STONES[nstones]:
+            self.game_state = self.game_state.apply_move(
+                gtp_position_to_coords(stone))
+        return response.success()
+
+    def handle_quit(self):
+        self._stopped = True
+        return response.success()
+
+    def handle_clear_board(self):
+        self.game_state = GameState.new_game(19)
+        return response.success()
+
+    def handle_known_command(self, command_name):
+        return response.bool_response(command_name in self.handlers.keys())
+
+    @staticmethod
+    def handle_boardsize(self, size):
+        if int(size) != 19:
+            return response.error('Only 19x19 currently supported, requested {}'.format(size))
+        return response.success()
+
+    def handle_showboard(self):
+        print_board(self.game_state.board)
+        return response.success()
+
+    @staticmethod
+    def handle_time_left(self, color, time, stones):
+        # TODO: Arguments: color color, int time, int stones
+        return GTPFrontend.not_implemented_but_respond_success("time_left")
+
+    @staticmethod
+    def handle_time_settings(self, main_time, byo_yomi_time, byo_yomi_stones):
+        # TODO: Arguments: int main_time, int byo_yomi_time, int byo_yomi_stones
+        return GTPFrontend.not_implemented_but_respond_success("time_settings")
+
+    @staticmethod
+    def handle_unknown(self, *args):
+        return response.error('Unrecognized command')
+
+    @staticmethod
+    def ignore(self, *args):
+        return response.success()
+
+    @staticmethod
+    def handle_protocol_version(self):
+        return response.success('2')
+
+    @staticmethod
+    def not_implemented_but_respond_success(self, fn_name):
+        print(f'Received {fn_name} command but it is not implemented by this frontend')
+        return response.success()
