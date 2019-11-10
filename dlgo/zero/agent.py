a2f54c4715306844d3533bff9eba6c775975e8a2
amrazek <amrazek@csu.fullerton.edu>
2019-11-10 07:32:56 -0800

    added ZeroTreeNode

---
+++ dlgo/zero/agent.py
@@ -0,0 +1,52 @@
+class Branch:
+    def __init__(self, prior):
+        self.prior = prior
+        self.visit_count = 0
+        self.total_value = 0.
+
+
+class ZeroTreeNode:
+    def __init__(self, state, value, priors, parent, last_move):
+        self.state = state
+        self.value = value
+        self.parent = parent
+        self.last_move = last_move
+        self.total_visit_count = 1
+        self.branches = {}
+
+        for move, p in priors.items():
+            if state.is_valid_move(move):
+                self.branches[move] = Branch(p)
+
+        self.children = {}
+
+    def moves(self):
+        return self.branches.keys()
+
+    def add_child(self, move, child_node):
+        self.children[move] = child_node
+
+    def has_child(self, move):
+        return move in self.children
+
+    def get_child(self, move):
+        return self.children[move]
+
+    def record_visit(self, move, value):
+        self.total_visit_count += 1
+        self.branches[move].visit_count += 1
+        self.branches[move].total_value += value
+
+    def expected_value(self, move):
+        branch = self.branches[move]
+
+        return branch.total_value / branch.visit_count if branch.visit_count > 0 else 0.0
+
+    def prior(self, move):
+        return self.branches[move].prior
+
+    def visit_count(self, move):
+        if move in self.branches:
+            return self.branches[move].visit_count
+
+        return 0
